773ca4f8d2c93b9d547cf1ffe53e23e2da719633
VijayKumar0422 <vijaykumar977129@gmail.com>
2026-06-11 13:37:54 +0530

    Function Include

---
+++ 07 FUNCTION/01 SIMPLE/evenOdd.py
@@ -0,0 +1,7 @@
+def check_num (num) :
+    if num % 2 == 0:
+        print ("Even")
+    else:
+        print ("Odd")
+
+check_num (1)
---
+++ 07 FUNCTION/01 SIMPLE/largest.py
@@ -0,0 +1,6 @@
+def find_largest (numbers) :
+    return max(numbers)
+
+nums = [10, 50, 20, 80, 30]
+
+print (find_largest(nums))
